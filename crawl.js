3e7473bba00d948d8833883d343a3c6cd8ca3b4c
deema <deemah.mehdawi@gmail.com>
2024-02-07 14:34:35 +0300

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,35 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody) //create an object rebresenting the html tree structure
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for(const linkElement of linkElements){
+        if(linkElement.href.slice(0,1)==='/'){
+            //This is a relative URL and we want to return the absolute
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`) //This will throw an error if the string is not valid URL
+                urls.push(`${baseURL}${linkElement.href}`)
+            }
+            catch(err){
+                console.log(`Error with relative url: ${err.message}`)
+            }
+        }
+        else{
+            //it starts with http so it is absolute
+            try{
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            }
+            catch(err){
+                console.log(`Error with absolute url: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
+
+
 function normlizeURL(urlString){
     const urlObj = new URL(urlString)
     const hostPath =  `${urlObj.hostname}${urlObj.pathname}`
@@ -9,5 +41,6 @@ function normlizeURL(urlString){
 
 // This will make the function available to use to other files
 module.exports = {
-    normlizeURL
+    normlizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,12 +1,11 @@
-const {normlizeURL} = require('./crawl.js')
+const {normlizeURL, getURLsFromHTML} = require('./crawl.js')
 const {test,expect} = require('@jest/globals')
-
+// run npm test to run the tests
 test('normlizeURL strip protocol', ()=>{
     const input = 'https://blog.boot.dev/path'
     const actual = normlizeURL(input)
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected) // I expect the actual output to equal the expected
-    // run npm test
 })
 
 test('normlizeURL strip trailing slash', ()=>{
@@ -20,5 +19,64 @@ test('normlizeURL capitals and http', ()=>{
     const input = 'http://BLOG.boot.dev/path'
     const actual = normlizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute', ()=>{
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "https://blog.boot.dev/path/"> Boot.dev Blog </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev/path/"
+    const actual = getURLsFromHTML(inputHTMLBody,inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', ()=>{
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "/path/"> Boot.dev Blog </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody,inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML multiple links', ()=>{
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "https://blog.boot.dev/path1/"> Boot.dev Blog </a>
+        </body>
+        <body>
+            <a href = "/path2/"> Boot.dev Blog </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody,inputBaseURL)
+    const expected = ["https://blog.boot.dev/path1/","https://blog.boot.dev/path2/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid url', ()=>{
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "invalid"> Boot.dev Blog </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody,inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
