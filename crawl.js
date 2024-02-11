db2754555da4a4feeae66556701356d0578095fb
Dimah <dimahmehdawi@gmail.com>
2024-02-11 21:45:19 +0300

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,27 +1,50 @@
-const {JSDOM} = require('jsdom')
+const { JSDOM } = require('jsdom')
 
-function getURLsFromHTML(htmlBody, baseURL){
+async function crawlPage(currentURL) {
+    console.log("Activley crawling:", currentURL)
+    try {
+        const resp = await fetch(currentURL);
+
+        if(resp.status >399){
+            console.log(`Error in fetch with status code ${resp.status} on page ${currentURL}`)
+            return;
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if(!contentType.includes("text/html")){
+            console.log(`non html response, content type: ${contentType} on page ${currentURL}`)
+            return;
+        }
+
+        const str = await resp.text()
+        console.log(str)
+    } catch (err) {
+        console.log("Error: Invalid URL",err)
+    }
+}
+
+function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody) //create an object rebresenting the html tree structure
     const linkElements = dom.window.document.querySelectorAll('a')
-    for(const linkElement of linkElements){
-        if(linkElement.href.slice(0,1)==='/'){
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
             //This is a relative URL and we want to return the absolute
-            try{
+            try {
                 const urlObj = new URL(`${baseURL}${linkElement.href}`) //This will throw an error if the string is not valid URL
                 urls.push(`${baseURL}${linkElement.href}`)
             }
-            catch(err){
+            catch (err) {
                 console.log(`Error with relative url: ${err.message}`)
             }
         }
-        else{
+        else {
             //it starts with http so it is absolute
-            try{
+            try {
                 const urlObj = new URL(linkElement.href)
                 urls.push(urlObj.href)
             }
-            catch(err){
+            catch (err) {
                 console.log(`Error with absolute url: ${err.message}`)
             }
         }
@@ -30,11 +53,11 @@ function getURLsFromHTML(htmlBody, baseURL){
 }
 
 
-function normlizeURL(urlString){
+function normlizeURL(urlString) {
     const urlObj = new URL(urlString)
-    const hostPath =  `${urlObj.hostname}${urlObj.pathname}`
-    if(hostPath.length>0 && hostPath.slice(-1)==='/'){
-        return hostPath.slice(0,-1)
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+        return hostPath.slice(0, -1)
     }
     return hostPath
 }
@@ -42,5 +65,6 @@ function normlizeURL(urlString){
 // This will make the function available to use to other files
 module.exports = {
     normlizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("hello")
+const {crawlPage} = require('./crawl.js')
+
+function main(){
+    if(process.argv.length<3){
+        console.log("no website provided")
+        process.exit(1)
+    }
+    if(process.argv.length>3){
+        console.log("Too many args provided")
+        process.exit(1)
+    }
+    /*for(const arg of process.argv){
+        console.log(arg)
+    }*/
+
+    const baseURL = process.argv[2];
+    console.log("starting crawl of", baseURL)
+    crawlPage(baseURL)
+}
+
+
+
+main();
